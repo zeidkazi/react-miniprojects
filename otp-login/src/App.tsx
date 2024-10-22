39b2912079589a0d1c02242760fdef9921a66e38
ZEID KAZI <zeidkazi7860@gmail.com>
2024-10-22 11:20:31 +0530

    Add: otp login boiler plate code

---
+++ otp-login/src/App.tsx
@@ -0,0 +1,11 @@
+import OtpContainer from "./components/OtpContainer";
+
+function App() {
+  return (
+    <div className="h-screen w-full flex items-center justify-center bg-gradient-to-b from-[#f2fcfe] to-[#3135fa]">
+      <OtpContainer />
+    </div>
+  );
+}
+
+export default App;
