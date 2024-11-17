7ec757c616d9ec98513f27ccd21c1b2641ae0285
ZEID KAZI <zeidkazi7860@gmail.com>
2024-11-17 23:27:42 +0530

    Modify navbar : controls to open sidebar

--- chatbot/src/components/Navbar.tsx
+++ chatbot/src/components/Navbar.tsx
@@ -2,14 +2,23 @@ import { Menu } from "lucide-react";
 import SpinnerLogo from "./SpinnerLogo";
 import { motion } from "motion/react";
 
-const Navbar = () => {
+interface NavbarTypes {
+  OpenSidebar: (input: boolean) => void;
+}
+
+const Navbar = ({ OpenSidebar }: NavbarTypes) => {
   return (
     <div className="w-full h-20 border-b flex items-center justify-between p-4 z-10 bg-white">
       <motion.div className="flex items-center justify-center gap-5">
-        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+        <motion.div
+          onClick={() => OpenSidebar(true)}
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="cursor-pointer"
+        >
           <Menu />
         </motion.div>
-        <p className="text-2xl font-bold leading-relaxed">
+        <p className="text-xl md:text-2xl font-bold leading-relaxed">
           Nebul
           <span className="bg-gradient-to-r from-purple-500 to-blue-500 font-[900] bg-clip-text text-transparent">
             AI
