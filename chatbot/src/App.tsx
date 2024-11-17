8176f0d7dc1636b750fa6bb968e9be4e53963937
ZEID KAZI <zeidkazi7860@gmail.com>
2024-11-17 23:28:23 +0530

    Api integration for Ai response

--- chatbot/src/App.tsx
+++ chatbot/src/App.tsx
@@ -1,16 +1,79 @@
+import { useState } from "react";
 import Background from "./components/Background";
 import InputField from "./components/InputField";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { AnimatePresence } from "motion/react";
+import Chatbox from "./components/Chatbox";
+
+export interface MessageType {
+  role: string;
+  message: string;
+}
+
+export interface getResponseType {
+  // e:
+  //   | React.MouseEvent<HTMLButtonElement>
+  //   | React.KeyboardEvent<HTMLInputElement>;
+  input: string;
+}
 
 const App = () => {
+  const [isSidebar, setIsSidebar] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [messages, setMessages] = useState<MessageType[]>([]);
+
+  const getResponse = async ({ input }: getResponseType) => {
+    setIsLoading(true);
+    const userMessage = { role: "USER", message: input };
+    setMessages((prev) => [...prev, userMessage]);
+    try {
+      const response = await fetch("https://api.cohere.com/v1/chat", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${
+            import.meta.env.VITE_REACT_APP_COHERE_API_KEY
+          }`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          message: input,
+          model: "command-r-08-2024",
+          preamble:
+            "You are an AI-assistant chatbot. You are trained to assist users by providing thorough and helpful responses to their queries.",
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      const chatBotMessage = {
+        role: "CHATBOT",
+        message: data?.text,
+      };
+      setMessages((prev) => [...prev, chatBotMessage]);
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error:", error);
+      setIsLoading(false);
+    }
+  };
+  console.log(messages);
+
   return (
     <>
-      <div className="min-h-screen w-full flex flex-col font-geist">
-        <Navbar />
-        <div className="w-full flex-1 ">
+      <div className="h-screen max-h-screen w-full flex flex-col font-geist ">
+        <Navbar OpenSidebar={setIsSidebar} />
+        <AnimatePresence mode="wait">
+          {isSidebar && <Sidebar OpenSidebar={setIsSidebar} />}
+        </AnimatePresence>
+        <div className="w-full flex-1 overflow-hidden flex flex-col">
           <Background />
+          <Chatbox messages={messages} isLoading={isLoading} />
         </div>
-        <InputField />
+        <InputField getResponse={getResponse} />
       </div>
     </>
   );
