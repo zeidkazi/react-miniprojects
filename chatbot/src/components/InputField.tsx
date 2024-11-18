b195b0634c46cb339660885038aa2eeb402441d8
ZEID KAZI <zeidkazi7860@gmail.com>
2024-11-18 20:37:49 +0530

    Add: keydown fucntion on textarea

--- chatbot/src/components/InputField.tsx
+++ chatbot/src/components/InputField.tsx
@@ -29,15 +29,31 @@ const InputField = ({ getResponse }: InputType) => {
     setInputText(e.target.value);
   };
 
+  const keyDownFunction = (
+    e: React.KeyboardEvent<HTMLTextAreaElement>
+  ): void => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleSubmit();
+    }
+  };
+
+  const handleSubmit = () => {
+    getResponse({ input: inputText });
+    setInputText("");
+  };
+
   return (
     <motion.div
       layout
       className="w-full min-h-20 max-h-44 border-t flex items-center justify-between p-4 z-10 bg-white"
     >
       <textarea
+        onKeyDown={keyDownFunction}
         ref={textareaRef}
         rows={1}
         onChange={handleOnChange}
+        value={inputText}
         className="w-full p-2 border-[2px] rounded-2xl appearance-none resize-none focus:outline-none focus:border-pink-200 focus:placeholder:text-pink-300 transition-colors duration-300 ease-in-out overflow-y-auto"
         placeholder="Ask NebulAI ..."
         style={{ minHeight: "48px", maxHeight: "160px" }}
@@ -45,9 +61,10 @@ const InputField = ({ getResponse }: InputType) => {
       <motion.button
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
-        className="p-2 text-gray-400 hover:text-pink-300 transition-colors delay-100 ease-in ml-2 flex-shrink-0"
+        onClick={handleSubmit}
+        className="p-2 text-gray-400 hover:text-pink-300 transition-colors ease-in ml-2 flex-shrink-0"
       >
-        <Send onClick={() => getResponse({ input: inputText })} />
+        <Send />
       </motion.button>
     </motion.div>
   );
