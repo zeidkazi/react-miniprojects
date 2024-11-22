de18e4128c817f92db8890dc7e34d7fe46f9c9fa
ZEID KAZI <zeidkazi7860@gmail.com>
2024-11-22 22:42:32 +0530

    Add: drawer component

---
+++ github_passport/src/components/Drawer.tsx
@@ -0,0 +1,23 @@
+import { motion } from "motion/react";
+
+interface DrawerPropType {
+  setDrawer: (arg: boolean) => void;
+}
+
+const Drawer = ({ setDrawer }: DrawerPropType) => {
+  return (
+    <div
+      onClick={() => setDrawer(false)}
+      className="absolute inset-0 bg-black/20 flex items-end justify-center"
+    >
+      <motion.div
+        initial={{ y: 500 }}
+        animate={{ y: 0 }}
+        exit={{ y: 500 }}
+        className="bg-white  w-full h-96"
+      ></motion.div>
+    </div>
+  );
+};
+
+export default Drawer;
