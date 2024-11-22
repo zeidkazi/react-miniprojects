49a4f47c1ed1370284abaf8fa792c7f302d4cbd4
ZEID KAZI <zeidkazi7860@gmail.com>
2024-11-22 22:42:23 +0530

    Add: background

---
+++ github_passport/src/components/Background.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Background = () => {
+  return (
+    <div className="absolute bg-white h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)] pointer-events-none -z-10"></div>
+  );
+};
+
+export default Background;
